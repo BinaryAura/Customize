07b2247c902bf77dbe6a67066dd09e24e857044c
BinaryAura <jadunker@hotmail.com>
2015-10-30 16:01:38 -0500

    Initial Setup

---
+++ src/main/java/net/binaryaura/customize/Customize.java
@@ -0,0 +1,21 @@
+package net.binaryaura.customize;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Customize.MODID, name = Customize.NAME, version = Customize.VERSION)
+public class Customize
+{
+	public static final String NAME = "Customize";
+    public static final String MODID = "customize";
+    public static final String VERSION = "1G0-1";
+    
+    
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
